df60da453b9d0341e642bda8497dab8982e386bc
Aquib Tahyat <123585282+aquibtahyat@users.noreply.github.com>
2026-01-27 18:09:15 +0600

    refactor: used app constants for method channel constants

---
+++ device_vitals_app/lib/src/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String methodChannel = 'com.example.device_vitals';
+  static const String getThermalStateMethod = 'getThermalState';
+  static const String getBatteryLevelMethod = 'getBatteryLevel';
+  static const String getMemoryUsageMethod = 'getMemoryUsage';
+}
--- device_vitals_app/lib/src/features/device_vitals/data/data_sources/platform/platform_data_source.dart
+++ device_vitals_app/lib/src/features/device_vitals/data/data_sources/platform/platform_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:device_vitals_app/src/core/constants/app_constants.dart';
 import 'package:device_vitals_app/src/features/device_vitals/data/models/battery_level_model.dart';
 import 'package:device_vitals_app/src/features/device_vitals/data/models/memory_usage_model.dart';
 import 'package:device_vitals_app/src/features/device_vitals/data/models/thermal_state_model.dart';
@@ -6,22 +7,28 @@ import 'package:injectable/injectable.dart';
 
 @lazySingleton
 class PlatformDataSource {
-  static const _channel = MethodChannel('com.example.device_vitals');
+  static final _channel = MethodChannel(AppConstants.methodChannel);
 
   Future<ThermalStateModel> getThermalStatus() async {
-    final result = await _channel.invokeMethod<int>('getThermalStatus');
+    final result = await _channel.invokeMethod<int>(
+      AppConstants.getThermalStateMethod,
+    );
 
     return ThermalStateModel(value: result);
   }
 
   Future<BatteryLevelModel> getBatteryLevel() async {
-    final result = await _channel.invokeMethod<int>('getBatteryLevel');
+    final result = await _channel.invokeMethod<int>(
+      AppConstants.getBatteryLevelMethod,
+    );
 
     return BatteryLevelModel(batteryLevel: result);
   }
 
   Future<MemoryUsageModel> getMemoryUsage() async {
-    final result = await _channel.invokeMethod<int>('getMemoryUsage');
+    final result = await _channel.invokeMethod<int>(
+      AppConstants.getMemoryUsageMethod,
+    );
 
     return MemoryUsageModel(memoryUsage: result);
   }
