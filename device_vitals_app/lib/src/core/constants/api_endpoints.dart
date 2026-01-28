a0f237a2ba24c038dc15988bd57fd7656dc31c7f
Aquib Tahyat <123585282+aquibtahyat@users.noreply.github.com>
2026-01-28 19:14:17 +0600

    feat: create api endpoints class

--- device_vitals_app/lib/src/core/constants/api_endpoints.dart
+++ device_vitals_app/lib/src/core/constants/api_endpoints.dart
@@ -1,5 +1,8 @@
 class ApiEndpoints {
   ApiEndpoints._();
-
   static const String baseUrl = 'https://sevenf576ecb3903.onrender.com';
+  static const String logDeviceVitals = '/api/vitals';
+  static const String getDeviceVitalsHistory = '/api/vitals/{device_id}';
+  static const String getDeviceVitalsAnalytics =
+      '/api/vitals/analytics/{device_id}';
 }
